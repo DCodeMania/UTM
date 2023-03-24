d0c69d173b2307d9373b73ed24d74dad17404052
DCodeMania <wolfmania44@gmail.com>
2023-03-24 11:25:35 +0530

    Updated

--- utm.js
+++ utm.js
@@ -2,4 +2,5 @@ if (window.location.search) {
   const po = Object.fromEntries(new URLSearchParams(window.location.search));
   const fd = document.getElementById("wf-form-Quote-Form");
   fd.innerHTML = Object.entries(po).map(([k, v]) => `<input type="hidden" name="${k}" value="${v}">`).join("");
+  console.log(po);
 }
