2619c125230b5d32eb642a7214ff4680b6301393
DCodeMania <wolfmania44@gmail.com>
2023-03-24 11:23:18 +0530

    first commit

---
+++ utm.js
@@ -0,0 +1,5 @@
+if (window.location.search) {
+  const po = Object.fromEntries(new URLSearchParams(window.location.search));
+  const fd = document.getElementById("wf-form-Quote-Form");
+  fd.innerHTML = Object.entries(po).map(([k, v]) => `<input type="hidden" name="${k}" value="${v}">`).join("");
+}
